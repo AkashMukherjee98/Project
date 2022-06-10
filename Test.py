48e4d20eccac3a0caa1e306419bf4974e6689c9c
AkashMukherjee98 <akash.mukherjee@cbnits.com>
2022-06-10 18:07:42 +0530

    Third Commit

--- Test.py
+++ Test.py
@@ -1,26 +1,93 @@
-from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from dotenv import load_dotenv
+from sqlalchemy.orm import joinedload
+
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/alchemy'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/alchemy'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db=SQLAlchemy(app)
+load_dotenv('.env')
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), nullable=False)
-    date = db.Column(db.Date, default=datetime.utcnow)
+
+
+@app.route('/')
+def dummy_api():
+    return "Welcome To Flask-SqlAlchemy"
+
+@app.route('/input', methods=['GET'])
+def insert():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        data = request.get_json()
+        user_name=data['username']
+        print(user_name)
+        user_email=data['email']
+        print(user_email)
+        user=User(username=user_name, email=user_email)
+        #print(user)
+        db.session.add(user)
+        db.session.commit()         
+    return "insert data"
+
+
+@app.route('/fetch', methods=['GET'])
+def fetch():
+    #return "hi"
+    val=[]
+    #value={}
+    users=User.query.all()
+    for user in users:
+        dic={}
+        id=user.id
+        name=user.username
+        mail=user.email
+        #print(id," ",name," ",mail)
+        dic=id,name,mail
+        #print("ss",dic)
+        val.append(dic)
+    print("\n")
+        #print(val)
+    return jsonify(val)
+    #for i in val:        
+    #return "ok  "
+
+@app.route('/update', methods=['PUT'])
+def update():
+    content_type = request.headers.get('Content-Type')
+    if(content_type == 'application/json'):
+        data = request.get_json()
+        u_id=data['id']
+        u_name=data['username']
+        u_mail=data['email']
+        print(u_id)
+        user = User.query.get(u_id)
+        user.username = u_name
+        user.email= u_mail
+        db.session.commit() 
+        return "update Sucessful"
+
+@app.route('/delt', methods=['DELETE'])
+def delt():
+    content_type = request.headers.get('Content-Type')
+    if(content_type == 'application/json'):
+        data = request.get_json()
+        u_id=data['id']
+        print(u_id)
+        user=User.query.get(u_id)
+        db.session.delete(user)
+        db.session.commit()
+        return "delete successful"
 
 
 
-'''
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'alchemy'
 
-'''
+if __name__== "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5001")
